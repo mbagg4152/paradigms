1e4f13d4beb3217f683d446e1d53e447283165d6
mag <hort4152@kettering.edu>
2020-02-03 12:45:36 -0500

    charcount

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -2,6 +2,7 @@
  * working with strings.
  * scanf is not appropriate for input, use fgets
  */
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,59 +12,51 @@
 #define IN 1
 
 void mkNull(char *s);
-void alphaCount();
-void reverseOrder();
-unsigned countWords(char *str);
+unsigned alphaCount(char *str);
+void reverseOrder(char *str);
+unsigned wordCount(char *str);
 
 int main(){
 	char input[40];
-
 	printf("enter string: \n");
 	fgets(input, 20, stdin);
-	printf("input: %s\n", input);
-	printf("Word count: %u\n", countWords(input));
-	alphaCount();
-
-	reverseOrder();
+	printf("Word count: %u\n", wordCount(input));
+	printf("Letter count: %u\n", alphaCount(input));
+	reverseOrder(input);
 	return 0;
 }
 
 // initialize a string to the empty string
-void mkNull(char *s){
-	*s = '\0';
-}
+void mkNull(char *s){ *s = '\0'; }
 
 
-unsigned countWords(char *str){
+unsigned wordCount(char *str){
 	int state = OUT;
-	unsigned wc = 0;                      // word count
-
-	// Scan all characters one by one
+	unsigned wc = 0;
+	unsigned aCount = 0;
 	while (*str) {
-		// If next character is a separator, set the
-		// state as OUT
-		if ((*str == ' ')|| (*str == '\n')|| (*str == '\t'))
+		if ((*str == ' ')|| (*str == '\n')|| (*str == '\t')) // If next character is a separator, set the state as OUT
 			state = OUT;
-
-		// If next character is not a word separator and
-		// state is OUT, then set the state as IN and
-		// increment word count
-		else if (state == OUT) {
+		else if (state == OUT) { // If next character is not a word separator and state is OUT, then set the state as IN and increment word count
 			state = IN;
 			++wc;
 		}
-
-		// Move to next character
 		++str;
 	}
-
 	return wc;
 }
 
-void alphaCount(){
-	//printf("fun alphaCount\n");
+unsigned alphaCount(char *str){
+	int state = OUT;
+	unsigned aCount = 0;
+	while (*str) {
+		if (isalpha(*str)) ++aCount;
+		++str;
+	}
+	return aCount;
+
 }
 
-void reverseOrder(){
+void reverseOrder(char *str){
 	//printf("fun reverseOrder\n");
 }

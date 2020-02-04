780eb09813aab25c98aa719c0f9f9d5cc5886c51
mag <hort4152@kettering.edu>
2020-02-03 23:38:01 -0500

    k

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -1,31 +1,40 @@
-/*
- * working with strings.
- * scanf is not appropriate for input, use fgets
+/* Maggie Horton
+ * CS-231 - Lab 3
+ * lab3.c - working with strings
+ * ------------------------------
+ * ALGORITHM
+ * ------------------------------
+ * [EXTERN. DATA TABLE]
+ * REG_CHAR		flag used for word count for non separator char
+ * SP_CHAR 		flag used for word count for separator char
+ * STR_LEN		initializer for array size, string lenth
+ * STR_NUM		initializer for array size, string count
  */
+
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define REG_CHAR 1 // flag used for word count for non separator char
-#define SP_CHAR 0 // flag used for word count for separator char
+#define REG_CHAR 1
+#define SP_CHAR 0
+#define STR_LEN 40
+#define STR_NUM 40
 
 unsigned alphaCount(char *inLine);
+unsigned charCheck(char *check);
 unsigned wordCount(char *inLine);
-void mkNull(char *s);
 void output(unsigned words, unsigned alphas);
 void reverseOrder(char *inLine);
-unsigned charCheck(char *toCheck);
 
 int main(){
 	char input[40];
 	printf("enter string: \n");
 	fgets(input, 20, stdin);
-	output(wordCount(input),alphaCount(input));
+	output(wordCount(input), alphaCount(input));
 	reverseOrder(input);
 	return 0;
 }
-
 
 void output(unsigned words, unsigned alphas){
 	printf("Word count: %u\n", words);
@@ -46,8 +55,8 @@ unsigned wordCount(char *inLine){
 	return wordCount;
 }
 
-unsigned charCheck(char *toCheck){
-	return (*toCheck == ' ') || (*toCheck == '\n') || (*toCheck == '\t');
+unsigned charCheck(char *check){
+	return (*check == ' ') || (*check == '\n') || (*check == '\t') || (*check == '\r');
 }
 
 unsigned alphaCount(char *inLine){
@@ -57,9 +66,29 @@ unsigned alphaCount(char *inLine){
 		++inLine;
 	}
 	return alphaCnt;
-
 }
 
 void reverseOrder(char *inLine){
-	//printf("fun reverseOrder\n");
+	char words[STR_NUM][STR_LEN];
+	char *token;
+	token = strtok(inLine, " ");
+	int c = 0;
+	while(token != NULL) {
+		printf("adding: %s\n", token);
+		//strcpy(token, words[c]);
+		strcpy(words[c], token);
+		token = strtok(NULL, " ");
+		++c;
+	}
+
+	int i = 0;
+	while(words[c]) {
+		printf("in arr: %s\n", words[i]);
+		++i;
+	}
+
+
+
+
+
 }

254ae0462bcbd7178e224a9665477997f7e8d304
mag <mag@budgie-buddie>
2020-02-06 10:49:30 -0500

    Update CMakeLists.txt, compare.c, and 5 more files...

--- lab4/compare.c
+++ lab4/compare.c
@@ -2,3 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+int main(){
+    return 0;
+}
--- lab4/lex.c
+++ lab4/lex.c
@@ -2,3 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int main(){
+    return 0;
+}

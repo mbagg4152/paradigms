00fb93ab2b3626f0b8161670034e361e1cc68204
mag <hort4152@kettering.edu>
2020-02-17 11:55:29 -0500

    .

--- lab4/spellCheck.c
+++ lab4/spellCheck.c
@@ -5,63 +5,45 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define DICT_LEN 1280
-#define WORD_LEN 40
-#define L 400
-
-void fileTest(char *inName, char *dName);
+void fileTest(char *fName);
+void quit(char *message, int code);
 
 int main(int argc, char *argv[]) {
-    if ((argc > 3) || (argc < 3)) {
-        printf("incorrect arg count. usage: ./s <file>.txt <dict>.txt\n");
-        exit(0);
-    }
+    if ((argc > 3) || (argc < 3)) { quit("incorrect arg count. usage: ./s <file>.txt <dict>.txt", 0); }
     char *inFName = argv[1], *dFName = argv[2];
     printf("input file: %s, dictionary: %s\n", inFName, dFName);
-    fileTest(inFName, dFName);
-
+    //fileTest(inFName);
+    //fileTest(dFName);
 }
 
-void fileTest(char *inName, char *dName) {
-    char inLines[L][L];
-    int index = 0;
-
-    FILE *dFile = fopen(dName, "r");
-
-    if (dFile == NULL) {
-        printf("error opening file(s)\n");
-        exit(0);
+void fileTest(char *fName) {
+    FILE *file = fopen(fName, "r");
+    if (file == NULL) { quit("error opening file(s)", 0); }
+    fseek(file, 0, SEEK_END);
+    long fSize = ftell(file);
+    rewind(file);
+    char *lines = (char *) malloc(fSize);
+    int lineCount = 0, counter = 0;
+    for (int ch = getc(file); ch != EOF; ch = getc(file)) {
+        if (isupper(ch)) ch = tolower(ch);
+        if (ch == '\n') { lineCount++; }
+        lines[counter] = ch;
+        counter++;
     }
 
-    fseek(dFile, 0, SEEK_END);
-    long fSize = ftell(dFile);
-    rewind(dFile);
-    char *dLines = (char *) malloc(fSize);
-    int lineCount = 0;
-    int n = 0;
-    for (int c = getc(dFile); c != EOF; c = getc(dFile)) {
-        dLines[n] = c;
-        n++;
-        if (c == '\n') lineCount++;
+    char *delim = "\n", *tokenized = strtok(lines, delim);
+    char *lineArr[lineCount];
+    int pos = 0;
+    while (tokenized != NULL) {
+        lineArr[pos++] = tokenized;
+        tokenized = strtok(NULL, delim);
     }
+    for (pos = 0; pos < lineCount; pos++) printf("%s\n", lineArr[pos]);
+    fclose(file);
+}
 
-
-    //printf("%s\n", dLines);
-    printf("%d lines in file\n", lineCount);
-    printf("file size %ld \n", fSize);
-    int avg = (int) fSize / lineCount;
-    printf("avg word length: %i\n", avg);
-    char *p = strtok(dLines, "\n");
-    char *arr[lineCount];
-    int i = 0;
-    while (p != NULL) {
-        arr[i++] = p;
-        p = strtok(NULL, "\n");
-    }
-    for (i = 0; i < lineCount; i++) {
-        printf("%s\n", arr[i]);
-    }
-    printf("\n");
-    fclose(dFile);
+void quit(char *message, int code) {
+    printf("%s\n", message);
+    exit(code);
 }
 

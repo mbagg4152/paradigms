5623df04e381706d39fe71214fe322f86bfaad8f
mag <hort4152@kettering.edu>
2020-03-01 13:50:31 -0500

    .

--- Haskell/lab2/func_testing/tst.hs
+++ Haskell/lab2/func_testing/tst.hs
@@ -1,5 +1,16 @@
+ -- Not part of assigment, for testing code sections only
+ 
  module Main where
  
 
  import Data.List
  import Data.Maybe
+ 
+ main = do
+  let list = [3,77,8,23,2,6,8,80]
+  let min = minimum list
+  print "min " ++ show min
+  let idx = fromMaybe 0 (elemIndex min list)
+  let split = splitAt idx list
+  let sec = drop 1 (snd split)
+  print sec

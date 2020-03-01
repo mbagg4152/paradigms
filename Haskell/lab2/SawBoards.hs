810ab743734171e8e3ccce656cc73ef7a0ce452d
mag <hort4152@kettering.edu>
2020-03-01 11:03:05 -0500

    .

--- Haskell/lab2/SawBoards.hs
+++ Haskell/lab2/SawBoards.hs
@@ -1,6 +1,8 @@
 -- Maggie Horton
 -- CS-231
 -- Haskell Lab 2
+-- SawBoards.hs: converts a String of integers to a list of Int,
+-- processing to get a result of list of pairs (Int, Int)
 
  module SawBoards
  where
@@ -9,7 +11,11 @@
 
 
  -- code
- sawBoards :: String -> [(Int, Int)]
 
- sawBoards x = (square, square*x)
-             where square = x*x
+ -- select the smallest length value in each line. smallest = selected
+ -- for all remaining boards, saw off length equal to selected board
+ sawBoards :: String -> [(Int, Int)]
+ sawBoards input =
+    [(length, number)] where
+    length = 0
+    number = 1

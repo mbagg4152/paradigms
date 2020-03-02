4745006e5425f0e033c27d790274104a03528d7c
mag <hort4152@kettering.edu>
2020-03-01 22:22:52 -0500

    .

--- Haskell/lab2/SawBoards.hs
+++ Haskell/lab2/SawBoards.hs
@@ -5,43 +5,50 @@
 -- processing to get a result of list of pairs (Int, Int)
 
  module SawBoards where
- -- select the smallest length value in each line. smallest = selected
- -- for all remaining boards, saw off length equal to selected board
- 
-
  import Data.List
  import Data.Maybe
  
- 
+ -- this is the core function of this file/module.
+ -- takes in a string, which is read in from a file &
+ -- 'saws' and 'stacks' the boards and outputs the results
+ -- as a list of tuples
  sawBoards :: String -> [(Int, Int)]
  sawBoards inStr = out
         where
             inList = map read (words inStr) :: [Int] -- read in input & put into list
-            len = getLen inList
-            num = getNum inList
-            out = zip len num
+            len = getLen inList -- get a list of 'sizes' after doing the 'sawing'
+            num = getNum inList -- get a list of occurrences of each 'size' after doing the 'sawing'
+            out = zip len num -- create list of pairs/tuples from the lengths & numbers
 
+ 
+ -- returns a list of lengths after completing the 'sawing'
+ -- process
  getLen :: [Int] -> [Int]
  getLen [] = []
  getLen list = out
     where
-    out = [minimum list] ++ getLen (math list)
+    out = [minimum list] ++ getLen (updateList list)
  
+  -- returns a list of numbers after completing the 'sawing'
+  -- process
  getNum :: [Int] -> [Int]
  getNum [] = []
  getNum list = out
     where
-    out = [length list] ++ getNum (math list)
-    
- math :: [Int] -> [Int]
- math [] = []
- math list =
-    sawed
+    out = [length list] ++ getNum (updateList list)
+ 
+ 
+ -- takes in a list, finds the minimum value of the list, removes it,
+ -- subtracts said value from each number in the list & then returns
+ -- the list. this is the 'sawing' process
+ updateList :: [Int] -> [Int]
+ updateList [] = []
+ updateList list = sawed
     where
-        sel = minimum list
-        newList = filter (/= sel) list
+        sel = minimum list -- smallest val in list
+        newList = filter (/= sel) list -- make new list after removing all instances of the smallest val
         removedCnt = (length list) - (length newList) -- keep track of number of items removed
-        sawed = map (subtract sel) newList
+        sawed = map (subtract sel) newList -- subtract smallest val from each val in list
         
 
     

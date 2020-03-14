3c0c548d0219b98a6c1e08109672c816fe9f4847
mag <hort4152@kettering.edu>
2020-03-13 20:27:07 -0400

    .

--- Haskell/final/KenKen.hs
+++ Haskell/final/KenKen.hs
@@ -1,7 +1,27 @@
 module Main where
 
+import System.Environment
+import System.IO
+import Data.Char
+import Data.Function
+import Data.List
+
 main = do
-    putStrLn ""
-    
+    fname <- getArgs
+    fcontent <- readFile (fname!!0)
+    let flines = lines fcontent
+    putStrLn fcontent
+    let kSize = read ( flines!!0) :: Int
+    let kss = boardSize kSize
+    putStrLn ("board size: " ++ kss)
+
+
+
+boardSize :: Int -> String
+boardSize sze
+    | sze == 4 = "four"
+    | sze == 5 = "five"
+    | sze == 6 = "six"
+    | otherwise = "not a size"
 
 
